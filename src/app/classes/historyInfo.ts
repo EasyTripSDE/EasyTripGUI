078980bb69dfa4969dd1353c2ed4941b4e237f00
Carlin Nicola <nicola.carlin-1@studenti.unitn.it>
2023-01-27 15:14:21 +0100

    Improve history management

--- src/app/classes/historyInfo.ts
+++ src/app/classes/historyInfo.ts
@@ -2,11 +2,13 @@
 export class HistoryInfo{
     id: number;
     desc: string;
-    url: string
+    params: any;
+    url: string;
 
-    constructor (id: number, desc: string, url: string){
+    constructor (id: number, desc: string, url: string, params: any){
         this.id = id;
         this.desc = desc;
         this.url = url;
+        this.params = params;
     }
 }
--- src/app/component/history/history.component.ts
+++ src/app/component/history/history.component.ts
@@ -23,6 +23,28 @@ export class HistoryComponent implements OnInit{
     this.getHistory();
   }
 
+  async searchDest(event: any, url:any, arrayIndex: number){
+    event.preventDefault();
+    await lastValueFrom(this.http.get<any>('http://localhost:12349' + url).pipe(map(data => {
+      // @ts-ignore
+      this.router.navigateByUrl("/destination", {state: {"data": data.message, "param": this.destList[arrayIndex].params}});
+    }),catchError(error => {
+      console.log(error)
+      return of([]);
+    })));
+  }
+
+  async searchPath(event:any, url:any, arrayIndex: number){
+    event.preventDefault();
+    await lastValueFrom(this.http.get<any>('http://localhost:12349' + url).pipe(map(data => {
+      // @ts-ignore
+      this.router.navigateByUrl("/path", {state: {"data": data.message, "param": this.destList[arrayIndex].params}});
+    }),catchError(error => {
+      console.log(error)
+      return of([]);
+    })));
+  }
+
   async deleteElement(id:any){
     const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8').set('x-access-token', sessionStorage.getItem('token') ?? "");
     await lastValueFrom(this.http.delete<any>('http://localhost:12349/v1/history/' + id, { headers: headers }).pipe(map(data => {
@@ -36,8 +58,19 @@ export class HistoryComponent implements OnInit{
   async getHistory(){
     const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8').set('x-access-token', sessionStorage.getItem('token') ?? "");
     await lastValueFrom(this.http.get<any>('http://localhost:12349/v1/history', { headers: headers }).pipe(map(data => {
-      this.setPathList(data.history.travel);
-      this.setDestList(data.history.destination)
+      //this.setPathList(data.history.travel);
+    this.destList = [{
+      id: 0,
+      desc: "desc",
+      url: "/v1/trip/destination?address=Trento&weather=true&bikes=true&interest=tourism",
+      params: { address: "ciao"}
+    },{
+      id: 1,
+      desc: "desc",
+      url: "/v1/trip/destination?address=Verona&weather=true&bikes=true&interest=tourism",
+      params: {merda: "ciao2"}
+    }]
+      //this.setDestList(data.history.destination)
     }),catchError(error => {
       console.log(error)
       return of([]);
@@ -48,13 +81,16 @@ export class HistoryComponent implements OnInit{
     console.log(pathInfo)
     this.pathList = new Array(pathInfo.length);
     let txt= ""
+    let inte = undefined;
 
     for(let i = 0; i < pathInfo.length; i++){
       txt = "Start: " + pathInfo[i].parameters.start + " End: " + pathInfo[i].parameters.end + "\n";
       if(pathInfo[i].parameters.interests.length > 0){
+        inte = new Array(pathInfo[i].parameters.interests.length)
         txt += "Interests: ";
         for(let y = 0; y < pathInfo[i].parameters.interests.length; y++){
           txt += pathInfo[i].parameters.interests[y];
+          inte[y] = pathInfo[i].parameters.interests[y]
           if(y != pathInfo[i].parameters.interests.length - 1){
             txt +=",";
           }
@@ -75,9 +111,22 @@ export class HistoryComponent implements OnInit{
         txt += "Weather: yes ";
       }
       
-      //txt += "\n Options: limit " + pathInfo[i].parameters.limit + " min distance: " + pathInfo[i].parameters.minDistance + " max detour: " + pathInfo[i].parameters.maxDetour + " profile: " + pathInfo[i].parameters.profile; 
+      txt += "\n Options: limit " + pathInfo[i].parameters.limit + " min distance: " + pathInfo[i].parameters.minDistance + " max detour: " + pathInfo[i].parameters.maxDetour + " profile: " + pathInfo[i].parameters.profile; 
 
-      this.pathList[i] = new HistoryInfo(pathInfo[i].id ,txt,pathInfo[i].url);
+      const parameters = { "type": "travel",
+        "start": pathInfo[i].parameters.start,
+        "end": pathInfo[i].parameters.end,
+        "weather": pathInfo[i].parameters.weather,
+        "bikes": pathInfo[i].parameters.bike,
+        "interests": inte,
+        "limit": pathInfo[i].parameters.limit,
+        "minDistance": pathInfo[i].parameters.minDistance,
+        "maxDetour": pathInfo[i].parameters.maxDetour,
+        "profile": pathInfo[i].parameters.profile,
+        "route": pathInfo[i].parameters.route 
+      };
+
+      this.pathList[i] = new HistoryInfo(pathInfo[i].id ,txt,pathInfo[i].url, parameters);
     }
 
     console.log(this.pathList);
@@ -88,13 +137,16 @@ export class HistoryComponent implements OnInit{
 
     this.destList = new Array(destInfo.length);
     let txt= ""
+    let inte = undefined;
 
     for(let i = 0; i < destInfo.length; i++){
       txt = "Address: " + destInfo[i].parameters.address + "\n";
       if(destInfo[i].parameters.interests.length > 0){
+        inte = new Array(destInfo[i].parameters.interests.length)
         txt += "Interests: ";
         for(let y = 0; y < destInfo[i].parameters.interests.length; y++){
           txt += destInfo[i].parameters.interests[y];
+          inte[y] = destInfo[i].parameters.interests[y]
           if(y != destInfo[i].parameters.interests.length - 1){
             txt +=",";
           }
@@ -114,8 +166,15 @@ export class HistoryComponent implements OnInit{
       } else{
         txt += "Weather: yes ";
       }
+
+      const parameters = { "type": "destination",
+        "address": destInfo[i].parameters.address,
+        "weather": destInfo[i].parameters.weather,
+        "bikes": destInfo[i].parameters.bike,
+        "interests": destInfo[i].parameters.interests,
+        "route": destInfo[i].parameters.route};
       
-      this.destList[i] = new HistoryInfo(destInfo[i].id ,txt,destInfo[i].url);
+      this.destList[i] = new HistoryInfo(destInfo[i].id ,txt,destInfo[i].url, parameters);
     }
 
     console.log(this.destList);
